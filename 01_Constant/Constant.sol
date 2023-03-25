8c831229df7e60dbce6b2c59da6439defbfa8294
Li, Amazing Ang <d0u.gie@hotmail.com>
2023-03-25 15:56:45 +0800

    Update Constant.sol

--- 01_Constant/Constant.sol
+++ 01_Constant/Constant.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.13;
 
 contract Constant {
     uint256 public constant varConstant = 1000;
-    uint public immutable varImmutable = 1000;
-    uint public variable = 1000;
+    uint256 public immutable varImmutable = 1000;
+    uint256 public variable = 1000;
 }
