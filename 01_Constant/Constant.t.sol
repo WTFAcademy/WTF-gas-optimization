a077a6ca3dd26b3ab75d6b736d092cc0bd486b72
aaron <lixin53517@gmail.com>
2023-05-11 16:15:45 +0800

    Place different variables in different contracts to prevent excess gas consumption caused by different function selector positions

--- 01_Constant/Constant.t.sol
+++ 01_Constant/Constant.t.sol
@@ -1,24 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
 import "forge-std/Test.sol";
 import "./Constant.sol";
 
 contract ConstantTest is Test {
     Constant public constantContract;
+    Immutable public immutableContract;
+    Public public publicContract;
 
     function setUp() public {
         constantContract = new Constant();
+        immutableContract = new Immutable();
+        publicContract = new Public();
     }
 
-    function testConstant() public {
+    function testConstant() public view {
         constantContract.varConstant();
     }
 
-    function testImmutable() public {
-        constantContract.varImmutable();
+    function testImmutable() public view {
+        immutableContract.varImmutable();
     }
 
-    function testVariable() public {
-        constantContract.variable();
+    function testVariable() public view {
+        publicContract.variable();
     }
 }
