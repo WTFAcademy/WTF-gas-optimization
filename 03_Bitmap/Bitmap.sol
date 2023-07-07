946ac9427ffbc7f0a7b34fe818c72191e36bbb98
Li, Amazing Ang <d0u.gie@hotmail.com>
2023-07-07 16:04:39 +0800

    update chapter title of 13 14

--- 03_Bitmap/Bitmap.sol
+++ 03_Bitmap/Bitmap.sol
@@ -5,14 +5,16 @@ contract Bitmap {
     bool[8] implementationWithBool;
     uint8 implementationWithBitmap;
 
+    // Use bool array: 35729
     function setDataWithBoolArray(bool[8] memory data) external {
         implementationWithBool = data;
     }
 
+    // use Bitmap: 22366 gas
     function setDataWithBitmap(uint8 data) external {
         implementationWithBitmap = data;
     }
-
+    
     function readWithBoolArray(uint8 index) external returns (bool) {
         return implementationWithBool[index];
     }
